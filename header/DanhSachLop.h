f165de49ec92da47ae31d4bf739bc847441965e6
Nghy <phamminhnghi1812@gmail.com>
2023-02-22 09:35:57 +0700

    them file nay

---
+++ header/DanhSachLop.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#define MAX_DSL 500
+using namespace std;
+
+class Lop{
+    private:
+    string maLop;
+    string tenLop;
+    int nienKhoa;
+    public:
+    Lop(string maLop, string tenLop, int nienKhoa );
+
+    string getMaLop();
+    void setMaLop(string maLop);
+    string getTenLop();
+    void setTenLop(string tenlop);
+    int getNienKhoa();
+    void setNienKhoa(int nienKhoa);
+};
+
+
+class danhSachLopHoc{
+    private:
+    Lop *PTR[MAX_DSL];
+};
+int main(){
+    cout<<"Hello";
+}
