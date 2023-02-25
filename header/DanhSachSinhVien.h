ac7e457d0d4895d1f6c4be44390c2fc9d5398828
PhanHoaiNhan <87808802+hoainhanpro@users.noreply.github.com>
2023-02-25 20:27:22 +0700

    Them cac phuong thuc co ban

---
+++ header/DanhSachSinhVien.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class DanhSachSinhVien{
+    public:
+        struct SinhVien{
+            char MASV[15];
+            char HO[21];
+            char TEN[16];
+            bool Phai; // Nam = 0, Nu = 1
+            char password[21];
+            DTPtr DanhSachDiemThi; //Con trỏ quản lý danh sách điểm thi
+        };
+
+        struct SinhVienNode{
+            SinhVien data;
+            SinhVienNode *next;
+        };
+        typedef SinhVienNode *SVPtr;
+
+        DanhSachSinhVien();
+        ~DanhSachSinhVien();
+        void khoiTaoSVPtr(SVPtr *First);
+        void inSinhVienRaFile(SVPtr First, ofstream &out);
+}
