b60a2670bbd1359e0521369435b0453504780aed
Nghy <phamminhnghi1812@gmail.com>
2023-02-28 16:06:41 +0700

    Thêm 1 vài thứ

--- source/DanhSachLop.cpp
+++ source/DanhSachLop.cpp
@@ -1,23 +1,60 @@
 #include "../header/DanhSachLop.h"
 Lop::Lop(){
-    maLop = tenLop = "N/A";
-    nienKhoa = 0;
+    malop = tenlop = "N/A";
+    nienkhoa = 0;
 }
-Lop::setMaLop(string maLop){
-    this->maLop = maLop;
+void Lop::setMaLop(string malop){
+    this->malop = malop;
 }
-Lop::getMaLop(){
-    return maLop;
+string Lop::getMaLop(){
+    return malop;
 }
-Lop::setTenLop(string tenLop){
-    this->tenLop = tenLop;
+void Lop::setTenLop(string tenlop){
+    this->tenlop = tenlop;
 }
-Lop::getTenLop(){
-    return tenLop;
+string Lop::getTenLop(){
+    return tenlop;
 }
-Lop::setNienKhoa(int nienKhoa){
-    this->nienKhoa = nienKhoa;
+void Lop::setNienKhoa(int nienkhoa){
+    this->nienkhoa = nienkhoa;
 }
-Lop::getNienKhoa(){
-    return nienKhoa;
+int Lop::getNienKhoa(){
+    return nienkhoa;
 }
+danhSachLopHoc::danhSachLopHoc(){
+    soluong = -1;
+}
+// danhSachLopHoc::~danhSachLopHoc(){
+//     delete [] List;
+// }
+void ListClasses::init(){
+    for(int i=0;i<soluong;i++){
+        List[i] = new Lop();
+    }
+}
+void ListClasses::setSoLuong(int soluong){
+    this->soluong = soluong;
+}
+int ListClasses::getSoLuong(){
+    return soluong;
+}
+void ListClasses::insertList(){
+    string a; int b;
+    for(int i=0;i<soluong;i++){
+        printf("Nhap thong tin lop %d: \n",i+1);
+        cout<<"Ma lop: ";
+            getline(cin,a); \
+            List[i]->setMaLop(a);
+            cout<<endl;
+        cout<<"Ten lop: ";
+            getline(cin,a);
+            List[i]->setTenLop(a);
+            cout<<endl;
+        cout<<"Nien khoa: ";
+            cin>>b;
+            List[i]->setNienKhoa(b);
+            cout<<endl;
+    }
+}
+
+
