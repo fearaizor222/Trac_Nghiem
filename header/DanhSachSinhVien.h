7c87332fcfc9aa38f70e2b0828a00d6e9d2a6b85
Nghy <phamminhnghi1812@gmail.com>
2023-03-13 11:21:36 +0700

    Include more functions

--- header/DanhSachSinhVien.h
+++ header/DanhSachSinhVien.h
@@ -1,9 +1,6 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-
-#include "../header/DanhSachDiemThi.h"
-#include "../header/DanhSachDiemThi.h"
 #include "DanhSachDiemThi.h"
 using namespace std;
 
@@ -15,10 +12,7 @@ class DanhSachSinhVien{
             char TEN[16];
             bool Phai; // Nam = 0, Nu = 1
             char password[21];
-
-            DanhSachDiemThi::DTPtr DanhSachDiemThi; //Con trỏ quản lý danh sách điểm thi
             DanhSachDiemThi::DTPtr ptr; //Con trỏ quản lý danh sách điểm thi
-
         };
 
         struct SinhVienNode{
@@ -29,11 +23,13 @@ class DanhSachSinhVien{
 
         DanhSachSinhVien();
         ~DanhSachSinhVien();
-        void insertFirst(SVPtr &First, SinhVien sv);
-        void insertLast(SVPtr &First, SinhVien sv);
         void inSinhVienRaFile(SVPtr First);
+        void insertFirst(SinhVien sv);
+        void insertAfterSV(SVPtr First , SinhVien sv);
+        SVPtr &getFirst() { 
+            return First; 
+        }
     private:
-        int soluong;
-        SVPtr First = NULL;
-};
+        SVPtr First;
 
+};
