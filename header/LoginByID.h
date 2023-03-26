b14a1ddce9e60fbced3bfa4a379a9ca7fc080407
Nghy <phamminhnghi1812@gmail.com>
2023-03-26 15:26:43 +0700

    thêm hàm searchPW và 2 trường hợp hàm login

--- header/LoginByID.h
+++ header/LoginByID.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include "DanhSachLop.h"
 #include "../header/DanhSachSinhVien.h"
-extern bool search(DanhSachSinhVien::SVPtr FirstSV ,string masv);
+extern bool searchID(DanhSachSinhVien::SVPtr FirstSV ,string masv);
+extern bool searchPW(DanhSachSinhVien::SVPtr FirstSV, string password);
 extern void login(string tendangnhap, string matkhau);
 #endif
--- source/LoginByID.cpp
+++ source/LoginByID.cpp
@@ -1,8 +1,18 @@
 #include "../header/LoginByID.h"
 bool search(DanhSachSinhVien::SVPtr FirstSV,string masv) {
         DanhSachSinhVien::SVPtr current = FirstSV;
-        while (current != nullptr) {
-            if (current->sv_data == masv) {
+        while (current!=nullptr){
+            if(current->sv_data.MASV==masv) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+bool searchPW(DanhSachSinhVien::SVPtr FirstSV, string password){
+        DanhSachSinhVien::SVPtr current = FirstSV;
+        while(current!=nullptr){
+            if(current->sv_data.password==password){
                 return true;
             }
             current = current->next;
@@ -10,7 +20,16 @@ bool search(DanhSachSinhVien::SVPtr FirstSV,string masv) {
         return false;
     }
 void login(string tendangnhap, string matkhau){
+    DanhSachSinhVien::SVPtr FirstSV;
     if(tendangnhap=="GV" && matkhau=="GV"){
-        ListClasses::hieuChinh(); 
+
+    }else if(tendangnhap!="GV" || matkhau!="GV"){
+        if(searchID(FirstSV, tendangnhap)==false && searchPW(FirstSV, matkhau)==false){
+            cout<<"Ten dang nhap hoac mat khau khong dung"<<endl;
+        }else if(searchID(FirstSV,tendangnhap)==true && searchPW(FirstSV,matkhau)==false){
+            cout<<"Sai mat khau"<<endl;
+        }else if(searchID(FirstSV,tendangnhap)==true && searchPW(FirstSV,matkhau)==true){
+            
+        }
     }
 }
