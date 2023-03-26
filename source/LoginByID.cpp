a4770d1228c45de7ad9b476b214c339eb30a92fe
Nghy <phamminhnghi1812@gmail.com>
2023-03-26 16:09:22 +0700

    gộp 2 hàm search lại 1

--- source/LoginByID.cpp
+++ source/LoginByID.cpp
@@ -1,35 +1,22 @@
 #include "../header/LoginByID.h"
-bool search(DanhSachSinhVien::SVPtr FirstSV,string masv) {
+bool search(DanhSachSinhVien::SVPtr FirstSV,string masv, string matkhau) {
         DanhSachSinhVien::SVPtr current = FirstSV;
         while (current!=nullptr){
-            if(current->sv_data.MASV==masv) {
+            if(current->sv_data.MASV==masv && current->sv_data.password==matkhau) {
                 return true;
             }
             current = current->next;
         }
         return false;
     }
-bool searchPW(DanhSachSinhVien::SVPtr FirstSV, string password){
-        DanhSachSinhVien::SVPtr current = FirstSV;
-        while(current!=nullptr){
-            if(current->sv_data.password==password){
-                return true;
-            }
-            current = current->next;
-        }
-        return false;
-    }
+
 void login(string tendangnhap, string matkhau){
     DanhSachSinhVien::SVPtr FirstSV;
     if(tendangnhap=="GV" && matkhau=="GV"){
 
     }else if(tendangnhap!="GV" || matkhau!="GV"){
-        if(searchID(FirstSV, tendangnhap)==false && searchPW(FirstSV, matkhau)==false){
-            cout<<"Ten dang nhap hoac mat khau khong dung"<<endl;
-        }else if(searchID(FirstSV,tendangnhap)==true && searchPW(FirstSV,matkhau)==false){
-            cout<<"Sai mat khau"<<endl;
-        }else if(searchID(FirstSV,tendangnhap)==true && searchPW(FirstSV,matkhau)==true){
-            
-        }
+        if(search(FirstSV,tendangnhap,matkhau)==true){
+
+        }else cout<<"Ten dang nhap hoac matkhau khong dung"<<endl;
     }
 }
