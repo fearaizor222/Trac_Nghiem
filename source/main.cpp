cf6baa22a93f8caa363083204b06222475900b00
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2023-06-10 19:58:28 +0700

    idk

--- source/main.cpp
+++ source/main.cpp
@@ -10,14 +10,24 @@ void MainSceneSV(SinhVien *&sv, DanhSachMonHoc &dsmh, DanhSachCauHoi &dsch)
     int index = 0;
     SetWindowSize(MAIN_SV_WIDTH, MAIN_SV_HEIGHT);
     SetWindowPosition(GetMonitorWidth(0)/2 - MAIN_SV_WIDTH/2, GetMonitorHeight(0)/2 - MAIN_SV_HEIGHT/2);
-    InputBox input_ma_mon{{0, 0, 200, 50}, BLACK, BLUE, LIGHTGRAY, font, false};
+    InputBox input_ma_mon{{25, 25, 200, 50}, BLACK, BLUE, LIGHTGRAY, font, false};
+    InputBox box_thoi_gian_thi(Rectangle{MidCenter().x - 400 / 2 + 180, MidCenter().y - 300 / 2 + 110, 200, 50}, BLACK, BLACK, WHITE, font);
+    InputBox box_so_cau_hoi(Rectangle{MidCenter().x - 400 / 2 + 180, MidCenter().y - 300 / 2 + 170, 200, 50}, BLACK, BLACK, WHITE, font);
     Rectangle box = {5, 100, 1490, 600};
     Button next{{BotRight().x - 110, BotRight().y - 90, 100, 50}, "Next", WHITE, BLUE, font};
     Button prev{{BotRight().x - 220, BotRight().y - 90, 100, 50}, "Prev", WHITE, BLUE, font};
+
     bool is_button_next_pressed = false;
     bool is_button_prev_pressed = false;
+    bool press_flag = false;
+    string ma_mon_chon_thi = "";
+    string so_cau_thi = "";
+    string thoi_gian_thi = "";
+
+    global_mouse_pos = GetMousePosition();
 
     while(!is_close_icon_pressed){
+
         BeginDrawing();
             ClearBackground(WHITE);
 
@@ -27,7 +37,7 @@ void MainSceneSV(SinhVien *&sv, DanhSachMonHoc &dsmh, DanhSachCauHoi &dsch)
             if(is_button_prev_pressed && page > 1) page--;
             index = 7 * (page - 1);
 
-            input_ma_mon.run(global_mouse_pos, TopLeft, 150, 50);
+            input_ma_mon.run(global_mouse_pos);
             DrawRectangleLinesEx(box, 3, BLACK);
             if(input_ma_mon.text.data == "" && !input_ma_mon.clicked)
                 DrawTextEx(font, "Mã môn", (Vector2){input_ma_mon.box.x + 50, input_ma_mon.box.y + 10}, 30, 0, BLACK);
@@ -44,9 +54,15 @@ void MainSceneSV(SinhVien *&sv, DanhSachMonHoc &dsmh, DanhSachCauHoi &dsch)
             DrawLineEx({273, 100}, {273, 700}, 2, BLACK);
             DrawLineEx({1300, 100}, {1300, 700}, 2, BLACK);
 
-
-            for (int place = 0; index < 7 * page && index < dsmh.getLength(); index++, place++)
+            for (int place = 0, offset = 0; index < dsmh.getLength(); index++, place++)
             {
+                if(input_ma_mon.text.data != ""){
+                    if(strstr(dsmh[index].ma_mon_hoc, input_ma_mon.text.data.c_str()) == nullptr){
+                        offset++;
+                        continue;
+                    }
+                }
+                Rectangle rec = {5, 175 + 75 * place, 1490, 75};
                 DTPtr dt = nullptr;
                 dt = (*sv->diem)[string(dsmh[index].ma_mon_hoc)];
                 std::string output_mark;
@@ -56,10 +72,49 @@ void MainSceneSV(SinhVien *&sv, DanhSachMonHoc &dsmh, DanhSachCauHoi &dsch)
                 else{
                     output_mark = std::to_string(dt->data.Diem).substr(0, std::to_string(dt->data.Diem).find(".") + 2);
                 }
-                DrawTextEx(font, std::to_string(index + 1).c_str(), {(5 + 100) / 2 - (MeasureTextEx(font, std::to_string(index + 1).c_str(), 30, 3).x / 2), 199 + 75 * place}, 30, 3, BLACK);
-                DrawTextEx(font, dsmh[index].ma_mon_hoc, Vector2{100 + 5, 199 + 75 * place}, 30, 3, BLACK);
-                DrawTextEx(font, (char *)dsmh[index].ten_mon_hoc.c_str(), Vector2{273 + 5, 199 + 75 * place}, 30, 3, BLACK);
-                DrawTextEx(font, (char*)output_mark.c_str(), Vector2{(1490 + 1300)/2 - MeasureTextEx(font, (char*)output_mark.c_str(), 30, 3).x/2, 199 + 75 * place}, 30, 3, BLACK);
+                DrawTextEx(font, std::to_string(index + 1 - offset).c_str(), {(5 + 100) / 2 - (MeasureTextEx(font, std::to_string(index + 1 - offset).c_str(), 30, 3).x / 2), 199 + 75 * (place - offset)}, 30, 3, BLACK);
+                DrawTextEx(font, dsmh[index].ma_mon_hoc, Vector2{100 + 5, 199 + 75 * (place - offset)}, 30, 3, BLACK);
+                DrawTextEx(font, (char *)dsmh[index].ten_mon_hoc.c_str(), Vector2{273 + 5, 199 + 75 * (place - offset)}, 30, 3, BLACK);
+                DrawTextEx(font, (char*)output_mark.c_str(), Vector2{(1490 + 1300)/2 - MeasureTextEx(font, (char*)output_mark.c_str(), 30, 3).x/2, 199 + 75 * (place - offset)}, 30, 3, BLACK);
+
+                if(CheckCollisionPointRec(GetMousePosition(), rec)){
+                    DrawRectangleLinesEx(rec, 3, GREEN);
+
+                    if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !press_flag){
+                        if(output_mark == "Chưa thi"){
+                            press_flag = true;
+                            ma_mon_chon_thi = dsmh[index].ma_mon_hoc;
+                        }
+                    }
+                }
+            }
+
+            if(press_flag){
+                Vector2 measure_lable = MeasureTextEx(font, (char*)ma_mon_chon_thi.c_str(), 30, 5);
+                Rectangle main_popup = {MidCenter().x - 400 / 2, MidCenter().y - 300 / 2, 400, 300};
+                Rectangle close_button = {main_popup.x + 350, main_popup.y, 50, 50};
+                DrawRectangleRec(main_popup, WHITE);
+                DrawRectangleRec(close_button, RED);
+                DrawTextEx(font, "X", {close_button.x + 15, close_button.y + 10}, 30, 5, WHITE);
+                DrawRectangleLines(main_popup.x, main_popup.y, main_popup.width, main_popup.height, BLACK);
+                DrawLine(main_popup.x, main_popup.y + 50, main_popup.x + main_popup.width, main_popup.y + 50, BLACK);
+                DrawLine(main_popup.x + 350, main_popup.y, main_popup.x + 350, main_popup.y + 50, BLACK);
+                // DrawTextEx(font, (char *)message.c_str(), Vector2{main_popup.x + main_popup.width / 2 - measure_msg.x / 2, main_popup.y + main_popup.height / 2 - measure_msg.y / 2}, 30, 5, BLACK);
+                DrawTextEx(font, (char*)ma_mon_chon_thi.c_str(), Vector2{main_popup.x + main_popup.width / 2 - measure_lable.x / 2, (main_popup.y * 2 + 50) / 2 - measure_lable.y / 2}, 30, 5, BLACK);
+
+                DrawTextEx(font, "Thời gian:", Vector2{main_popup.x + 20, main_popup.y + 120}, 30, 5, BLACK);
+                DrawTextEx(font, "Số câu:", Vector2{main_popup.x + 20, main_popup.y + 180}, 30, 5, BLACK);
+
+                box_thoi_gian_thi.run(global_mouse_pos);
+                box_so_cau_hoi.run(global_mouse_pos);
+
+                if(CheckCollisionPointRec(global_mouse_pos, close_button)){
+                    DrawRectangleRec(close_button, Fade(RED, 0.5f));
+
+                    if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+                        press_flag = false;
+                    }
+                }
             }
         EndDrawing();
 
