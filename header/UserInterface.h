0ec5c2841d238e74cfb20e741f6983e1bab7a79c
PhanHoaiNhan <87808802+hoainhanpro@users.noreply.github.com>
2023-06-16 00:58:02 +0700

    Add ten ham vao userinterface

--- header/UserInterface.h
+++ header/UserInterface.h
@@ -145,6 +145,11 @@ struct Button{
 void LoginScene(DanhSachLopHoc &dslh, SinhVien *&sv);
 void MainSceneSV(SinhVien *&sv, DanhSachMonHoc &dsmh, DanhSachCauHoi &dsch, std::string &testing_subject, std::string &time, std::string &number_of_question);
 void TestingScene(SinhVien *&sv, DanhSachCauHoi &dsch,DanhSachMonHoc &dsmh, std::string &testing_subject, std::string &time, std::string &number_of_question);
+void GiaoDienDanhSachSinhVien(DanhSachLopHoc &dslh, string a);
+void GiaoDienDanhSachLop(DanhSachLopHoc &dslh);
+void in_SinhVien_1_Lop(SVPtr FirstSV, float &cur_page, Font font, string malop, DanhSachLopHoc &dslh);
+void inDanhSachMon(DanhSachMonHoc &dsmh, float &cur_page, int max_item, bool is_button_next_pressed, bool is_button_prev_pressed);
+void GiaoDienDanhSachMon (DanhSachMonHoc &dsmh);
 
 Rectangle Popup(std::string message, std::string lable);
 
